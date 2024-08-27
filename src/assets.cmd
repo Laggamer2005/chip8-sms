58f3915fc944cffaed3a4d5a34c98d687a6fe852
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-27 19:25:14 +0100

    Update assets.cmd

--- src/assets.cmd
+++ src/assets.cmd
@@ -1,5 +1,19 @@
+@echo off
+
 pushd images
+
+REM Convert the main graphics file
+
 bmp2tile.exe "graphics.png" -savetiles "graphics.png.tiles.zx7" -savepalette "palette.bin"
+
 bmp2tile.exe "graphics.png" -savetilemap "graphics.png.tilemap.bin"
-for %%f in (tiles*.png) do bmp2tile.exe "%%f" -tileoffset 1 -savetiles "%%f.tiles.zx7"
+
+REM Loop through each PNG file starting with "tiles" and process it
+
+for %%f in (tiles*.png) do (
+
+    bmp2tile.exe "%%f" -tileoffset 1 -savetiles "%%f.tiles.zx7"
+
+)
+
 popd
